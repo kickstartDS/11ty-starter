961d04c6edbaa1b1258ae0b149dac01667714688
fleven <franz.leven@kickstartds.com>
2023-11-03 16:01:10 +0100

    feat(about-us): edit content

--- src/about-us_paid.tsx
+++ src/about-us_paid.tsx
@@ -5,6 +5,7 @@ import { Text } from "@kickstartds/ds-agency/text";
 import { Gallery } from "@kickstartds/ds-agency/gallery";
 import { PageWrapper } from "./_includes/PageWrapper";
 import { Hero } from "@kickstartds/ds-agency/components/hero/index.js";
+import { ImageStory } from "@kickstartds/ds-agency/components/image-story/index.js";
 
 export const data = {
   title: "About Systemics, your Design System Agency",
@@ -24,20 +25,13 @@ export default (props) => (
           srcMobile:
             "/static/img/colleagues-work-office-using-computers-looking-aside.jpg",
         }}
+        overlay
         largeHeadline
         textPosition="below"
         headline="Crafting Digital Brilliance at Systemics"
         sub="Elevating Headless Experiences through Innovative Design Systems"
         text={`At Systemics, we specialize in creating bespoke design systems that bring harmony and consistency to your digital presence. Our expertise extends beyond design, as we seamlessly integrate headless CMS architecture, maximizing the value of both worlds. We work closely with you, understanding your brand's essence and user needs, to craft experiences that leave a lasting impact.`}
       />
-      {/* <CtaPaid
-        fullWidth
-        largeHeadline
-        align="left"
-        headline="Crafting Digital Brilliance at **Systemics**"
-        sub="Elevating Headless Experiences through Innovative Design Systems"
-        text={`At Systemics, we specialize in creating bespoke design systems that bring harmony and consistency to your digital presence. Our expertise extends beyond design, as we seamlessly integrate headless CMS architecture, maximizing the value of both worlds. We work closely with you, understanding your brand's essence and user needs, to craft experiences that leave a lasting impact.`}
-      /> */}
     </Section>
 
     <Section
@@ -46,43 +40,55 @@ export default (props) => (
         sub: "Our crystal clear mission:",
         switchOrder: true,
         width: "default",
-        align: "left",
+        align: "center",
       }}
       content={{
         width: "default",
-        align: "center",
+        align: "right",
       }}
-      style="accent"
+      style="horizontalGradient"
       width="wide"
     >
       <Text
         text={`**We believe** that well-crafted design systems are the cornerstone of exceptional user experiences. Our team is dedicated to providing clients with tailored solutions that bridge the gap between creativity and functionality, ultimately reshaping the way users engage with brands.`}
-        layout="singleColumn"
-      />
-    </Section>
-
-    <Section width="wide">
-      <Text
-        layout="multiColumn"
-        text={`
-### Benefit from the **power of kickstartDS**
-**as we do too!**  
-We strongly believe in using the best tools to bring our clients exceptional results. That's why we've integrated kickstartDS into our workflow. This code-first framework for design system creation is our secret sauce to saving time, without compromising on quality. kickstartDS allows us to fast-track the development process, ensuring you get a fully functional and polished design system in a fraction of the time it would traditionally take.
-
-### **Maximizing Value** for Our Clients
-We're not just building design systems; we're building value. By combining our expertise in design systems and headless CMS architecture, we deliver solutions that are not only visually appealing but also inherently functional. This approach ensures that your brand's identity remains consistent across platforms while your content distribution becomes more dynamic and efficient. We firmly believe that a harmonious blend of design systems and headless CMSs unlocks immense potential for businesses, propelling them ahead in the digital race.`}
+        style="highlight"
       />
     </Section>
 
     <Section
-      headline={{
-        text: "About & History",
+      inverted
+      content={{
+        mode: "list",
+        gutter: "large",
       }}
-      style="accent"
-      width="default"
+      style="bold"
+      width="wide"
     >
-      <Text
-        text={`Founded by passionate digital enthusiasts, Systemics emerged from the desire to reshape the digital realm. With a history spanning over a decade, we've continuously evolved to stay at the forefront of design systems, headless CMSs, and cutting-edge development practices. Our journey is one of innovation, collaboration, and a relentless pursuit of excellence.`}
+      <ImageStory
+        image={{
+          src: "/static/img/office-building.png",
+        }}
+        text={` ## Benefit from the **power of kickstartDS**
+**as we do too!**
+We strongly believe in using the best tools to bring our clients exceptional results. That's why we've integrated kickstartDS into our workflow. This code-first framework for design system creation is our secret sauce to saving time, without compromising on quality.
+
+kickstartDS allows us to fast-track the development process, ensuring you get a fully functional and polished design system in a fraction of the time it would traditionally take.
+
+## **Maximizing Value** for Our Clients
+We're not just building design systems; we're building value. By combining our expertise in design systems and headless CMS architecture, we deliver solutions that are not only visually appealing but also inherently functional.
+
+This approach ensures that your brand's identity remains consistent across platforms while your content distribution becomes more dynamic and efficient. We firmly believe that a harmonious blend of design systems and headless CMSs unlocks immense potential for businesses, propelling them ahead in the digital race.
+`}
+      />
+      <ImageStory
+        layout="imageLeft"
+        image={{
+          src: "/static/img/office-building.png",
+        }}
+        headline="About & History"
+        text={`Founded by passionate digital enthusiasts, Systemics emerged from the desire to reshape the digital realm. With a history spanning over a decade, we've continuously evolved to stay at the forefront of design systems, headless CMSs, and cutting-edge development practices.
+        
+Our journey is one of innovation, collaboration, and a relentless pursuit of excellence.`}
       />
     </Section>
 
